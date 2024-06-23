8cf7b82ec319909210c550a09716cd0acc0e027e
AJprogramming123 <jaimesandres2023@gmail.com>
2024-06-23 13:23:49 -0400

    Created inteface

---
+++ Pgm3/jaimesAinterface.java
@@ -0,0 +1,13 @@
+public interface jaimesAinterface
+{
+    public String personPantherID = "6289563";
+    public String personLastName = "Jaimes";
+    public String personFirstName = "Andres";
+
+    public void thePersonSchool(String person);
+    public void thePersonPantherID(String person);
+    public void thePersonLastName(String person);
+    public void thePersonFirstName(String person);
+
+
+}
