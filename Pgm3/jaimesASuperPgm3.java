d5f57ae61618f6f4f3fe8c1d246c16dbd3ce699d
AJprogramming123 <jaimesandres2023@gmail.com>
2024-06-23 21:59:03 -0400

    adding comments

--- Pgm3/jaimesASuperPgm3.java
+++ Pgm3/jaimesASuperPgm3.java
@@ -4,66 +4,27 @@ public class jaimesASuperPgm3
     {
     	System.out.printf("%d\n", value);
     
-    }
+    }//end of public void methodOne( int value )
 
 
     public void methodTwo( String value1, String value2 )
     {
         System.out.printf("I am super methodTwo\n");
     
-    }
+    }//end of public void methodTwo( String value1, String value2 )
 
 
     public void methodThree()
     {
         System.out.printf("I am super methodThree\n");
 
-    }
+    }//end of public void methodThree()
 
 
     public void callMethodOne( int value )
     {
         methodOne(value);
 
-    }
+    }//end of public void callMethodOne( int value )
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-}
+}//end of public class jaimesASuperpgm3
