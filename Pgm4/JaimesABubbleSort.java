121d9927063f492929bd2c3743fc49b9714ba789
AJprogramming123 <jaimesandres2023@gmail.com>
2024-07-16 12:19:04 -0400

    Made bubble sort

--- Pgm4/JaimesABubbleSort.java
+++ Pgm4/JaimesABubbleSort.java
@@ -2,4 +2,53 @@ package Pgm4;
 
 public class JaimesABubbleSort {
     
+
+
+    public static void bubbleSort(String[] myName)
+    {
+        int maxElement;
+        int index;
+        String temp;
+
+        for( maxElement = myName.length - 1; maxElement >= 0; maxElement--)
+        {
+            for(index = 0; index <= maxElement - 1; index++)
+            {
+                if(myName[index].compareTo(myName[index + 1]) > 0)
+                {
+                    temp = myName[index];
+                    myName[index] = myName[index + 1];
+                    myName[index + 1] = temp;
+            
+                }
+            }
+        }
+
+        displayName(myName);
+    }
+
+    public static void displayName(String array[])
+    {
+        for(String element : array)
+        {
+            System.out.printf("%s ", element);
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        String[] myName = {"Jaimes", "Andres"};
+
+        System.out.printf("Before bubble sort: ");
+        displayName(myName);
+
+        System.out.printf("\n\n");
+
+        System.out.printf("After bubble sort: ");
+        bubbleSort(myName);
+
+
+
+    }
+ 
 }
